afe1009dd0def191b7e5072cdfe8bcf130fe34dc
Elakiya-G <elakiyag222@gmail.com>
2026-08-11 16:01:25 +0530

    Create 520-DetectCapital

---
+++ 520-DetectCapital/520-DetectCapital.java
@@ -0,0 +1,15 @@
+// Last updated: 8/11/2026, 4:02:08 PM
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.equals(word.toUpperCase())){
+            return true;
+        }
+        if(word.equals(word.toLowerCase())){
+            return true;
+        }
+        if(Character.isUpperCase(word.charAt(0))&&word.substring(1).equals(word.substring(1).toLowerCase())){
+            return true;
+        }
+        return false;
+    }
+}
