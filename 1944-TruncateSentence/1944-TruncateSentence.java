e0323b5d216452fac3aa8f6163de527520ea9b16
Elakiya-G <elakiyag222@gmail.com>
2026-08-11 15:59:03 +0530

    Create 1944-TruncateSentence

---
+++ 1944-TruncateSentence/1944-TruncateSentence.java
@@ -0,0 +1,17 @@
+// Last updated: 8/11/2026, 3:59:45 PM
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                count++;
+                if (count == k) {
+                    return s.substring(0, i);
+                }
+            }
+        }
+
+        return s;
+    }
+}
