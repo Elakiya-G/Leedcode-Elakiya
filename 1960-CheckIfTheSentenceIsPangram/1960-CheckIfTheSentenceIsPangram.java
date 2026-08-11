f5cbb1222c3abead962ce7f0fc9bac0eb2dc9d72
Elakiya-G <elakiyag222@gmail.com>
2026-08-11 15:58:59 +0530

    Create 1960-CheckIfTheSentenceIsPangram

---
+++ 1960-CheckIfTheSentenceIsPangram/1960-CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,18 @@
+// Last updated: 8/11/2026, 3:59:42 PM
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        boolean[] seen = new boolean[26];
+
+        for (char ch : sentence.toCharArray()) {
+            seen[ch - 'a'] = true;
+        }
+
+        for (boolean b : seen) {
+            if (!b) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
