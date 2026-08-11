30b0a9d1a89a74049175268e6a02734dc2ab8962
Elakiya-G <elakiyag222@gmail.com>
2026-08-11 16:02:00 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,21 @@
+// Last updated: 8/11/2026, 4:02:43 PM
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+
+        // Count characters in magazine
+        for (char ch : magazine.toCharArray()) {
+            count[ch - 'a']++;
+        }
+
+        // Use characters for ransomNote
+        for (char ch : ransomNote.toCharArray()) {
+            if (count[ch - 'a'] == 0) {
+                return false;
+            }
+            count[ch - 'a']--;
+        }
+
+        return true;
+    }
+}
