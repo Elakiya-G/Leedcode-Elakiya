750c720b4390cec212342ed8f877db6a81e0438f
Elakiya-G <elakiyag222@gmail.com>
2026-08-11 15:58:56 +0530

    Create 1970-SortingTheSentence

---
+++ 1970-SortingTheSentence/1970-SortingTheSentence.java
@@ -0,0 +1,14 @@
+// Last updated: 8/11/2026, 3:59:39 PM
+class Solution {
+    public String sortSentence(String s) {
+        String[] words = s.split(" ");
+        String[] result = new String[words.length];
+
+        for (String word : words) {
+            int index = word.charAt(word.length() - 1) - '1';
+            result[index] = word.substring(0, word.length() - 1);
+        }
+
+        return String.join(" ", result);
+    }
+}
