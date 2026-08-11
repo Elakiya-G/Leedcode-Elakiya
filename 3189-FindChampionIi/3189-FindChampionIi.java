250409e53f5a33e2f8d27712865bbfc297bf6e4e
Elakiya-G <elakiyag222@gmail.com>
2026-08-11 15:58:29 +0530

    Create 3189-FindChampionIi

---
+++ 3189-FindChampionIi/3189-FindChampionIi.java
@@ -0,0 +1,23 @@
+// Last updated: 8/11/2026, 3:59:12 PM
+class Solution {
+    public int findChampion(int n, int[][] edges) {
+        int[] indegree = new int[n];
+
+        for (int[] edge : edges) {
+            indegree[edge[1]]++;
+        }
+
+        int champion = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                if (champion != -1) {
+                    return -1;
+                }
+                champion = i;
+            }
+        }
+
+        return champion;
+    }
+}
