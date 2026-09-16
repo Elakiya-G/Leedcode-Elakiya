050181ee814f011f01a52ed72f5724f88d6104d0
Elakiya-G <elakiyag222@gmail.com>
2026-09-16 09:42:16 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/version/java/1-TwoSum_20260916_094221.java
@@ -0,0 +1,15 @@
+// Last updated: 9/16/2026, 9:42:21 AM
+1import java.util.HashMap;
+2
+3class Solution {
+4    public int[] twoSum(int[] nums, int target) {
+5        for(int i=0;i<nums.length;i++){
+6            for(int j=i+1;j<nums.length;j++){
+7                if(nums[i]+nums[j]==target){
+8                    return new int[] {i,j};
+9            }
+10        }
+11        }
+12        return new int[] {};
+13    }
+14}
